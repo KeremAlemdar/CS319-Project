afa9c01b5ed0912c738e2b1b8cd048a7ffbffa9a
KeremAlemdar <kerem.alemdar.54.60@hotmail.com>
2021-04-25 16:01:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ronaac
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ronaac)
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./style.css";
+import MainPage from "./components/MainPage";
+import Login from "./components/Login";
+import Register from "./components/register";
+import GroupPage from "./components/GroupPage";
+import PollCreatePage from "./components/PollCreatePage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+      <PollCreatePage />
+    </div>
+    /*<Router>
+        <div className="app">
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/mainPage">
+              <MainPage />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>*/
+  );
+}
+
+export default App;
